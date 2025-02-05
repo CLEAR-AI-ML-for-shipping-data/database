bca14a3b23bca9af6d6dfff3468b8b25babe1039
Leon Boschman <leon.boschman@chalmers.se>
2025-02-05 11:18:56 +0100

    Format insert_ais_data.py

--- src/insert_ais_data.py
+++ src/insert_ais_data.py
@@ -1,64 +1,114 @@
-import pandas as pd
-from sqlalchemy.orm import Session
-from sqlalchemy import select, create_engine
+import argparse
+import json
+import os
 from datetime import datetime
-import csv, traceback
-from tqdm import tqdm
-import argparse, os, json
+from multiprocessing import Pool, cpu_count
 
-from db_schema import ClearAIS_DB, Ships, AIS_Data, Nav_Status
-from utils import find_files_in_folder, try_except
+import pandas as pd
+from tqdm import tqdm
+
+from db_schema import AIS_Data, ClearAIS_DB, Nav_Status, Ships
 from logger import getLogger
+from utils import find_files_in_folder, try_except
 
 logger = getLogger(__file__)
 
 
-csv_to_db_mapping = json.load(open("src/csv_to_db_mapping.json",'r'))
+csv_to_db_mapping = json.load(open("src/csv_to_db_mapping.json", "r"))
 
-from datetime import datetime
-dateparse = lambda x: datetime.strptime(x, "%d %b %Y %H:%M:%S %Z")
+
+def dateparse(datetime_str: str):
+    return datetime.strptime(datetime_str, "%d %b %Y %H:%M:%S %Z")
+
 
 @try_except(logger=logger)
 def read_and_transform_csv_chunk(file_path, chunk_size=10000):
     """
     Generator to read and transform CSV data in chunks and collect unique navigational statuses.
-    
+
     :param file_path: The path to the CSV file.
     :param chunk_size: Number of rows per chunk.
     """
     nav_status_set = set()
 
-    for chunk in pd.read_csv(file_path, chunksize=chunk_size, parse_dates=['Base station time stamp'], date_format=dateparse):
+    for chunk in pd.read_csv(
+        file_path,
+        chunksize=chunk_size,
+        parse_dates=["Base station time stamp"],
+        date_format=dateparse,
+    ):
         # Ensure the columns are named correctly
-        chunk.drop(chunk.columns[chunk.columns.str.contains('unnamed', case=False)], axis=1, inplace=True)
+        chunk.drop(
+            chunk.columns[chunk.columns.str.contains("unnamed", case=False)],
+            axis=1,
+            inplace=True,
+        )
 
         chunk = chunk.rename(columns=csv_to_db_mapping)
 
-
-        chunk.dropna(subset=['type_of_ship_and_cargo','type_of_ship', 'type_of_cargo','draught'], inplace=True)
-        chunk = chunk.astype({'mmsi':str, 'imo':str, 'type_of_ship':int, 'type_of_cargo':int, 'type_of_ship_and_cargo':int})
+        chunk.dropna(
+            subset=[
+                "type_of_ship_and_cargo",
+                "type_of_ship",
+                "type_of_cargo",
+                "draught",
+            ],
+            inplace=True,
+        )
+        chunk = chunk.astype(
+            {
+                "mmsi": str,
+                "imo": str,
+                "type_of_ship": int,
+                "type_of_cargo": int,
+                "type_of_ship_and_cargo": int,
+            }
+        )
         # Collect unique navigational statuses
-        nav_status_set.update(chunk['navigational_status_text'].unique())
+        nav_status_set.update(chunk["navigational_status_text"].unique())
 
-        ship_data_cols = ['mmsi', 'imo', 'size_a', 'size_b', 'size_c', 'size_d', 'type_of_ship', 'type_of_cargo', 'type_of_ship_and_cargo', 'draught']
+        ship_data_cols = [
+            "mmsi",
+            "imo",
+            "size_a",
+            "size_b",
+            "size_c",
+            "size_d",
+            "type_of_ship",
+            "type_of_cargo",
+            "type_of_ship_and_cargo",
+            "draught",
+        ]
         # Transform data into Ships table format
         ships_data = chunk.filter(items=ship_data_cols)
-        ships_data = ships_data.drop_duplicates(subset=["mmsi"]).to_dict(orient='records')
+        ships_data = ships_data.drop_duplicates(subset=["mmsi"]).to_dict(
+            orient="records"
+        )
 
         # Transform data into AIS_Data table format
-        ais_data_cols = ['timestamp', 'mmsi', 'latitude', 'longitude', 'navigational_status', 'navigational_status_text', 'speed_over_ground', 'heading','course_over_ground','country_ais', 'destination']
+        ais_data_cols = [
+            "timestamp",
+            "mmsi",
+            "latitude",
+            "longitude",
+            "navigational_status",
+            "navigational_status_text",
+            "speed_over_ground",
+            "heading",
+            "course_over_ground",
+            "country_ais",
+            "destination",
+        ]
         ais_data = chunk.filter(items=ais_data_cols).copy()
         # ais_data = ais_data.rename(columns={'Speed_over_ground':'speed'})
 
-        ## NOTE change this later to have nav staus as ID instead
-        # ais_data = ais_data.rename(columns={'Nav_status_text':'nav_status'}) 
+        # # NOTE change this later to have nav staus as ID instead
+        # ais_data = ais_data.rename(columns={'Nav_status_text':'nav_status'})
 
-        
         # Additional fields required for the AIS_Data table can be set to default or calculated values if not available in the CSV
-        fileds_list = ['course_over_ground', 'heading', 'destination','rot', 'eot']
 
-        ais_data = ais_data.to_dict(orient='records')
-        
+        ais_data = ais_data.to_dict(orient="records")
+
         yield ships_data, ais_data, nav_status_set
 
 
@@ -66,7 +116,7 @@ def read_and_transform_csv_chunk(file_path, chunk_size=10000):
 def bulk_insert_data_chunked(file_path, database_url, chunk_size=10000):
     """
     Function to bulk insert data from CSV file into the database in chunks and handle navigational statuses.
-    
+
     :param file_path: The path to the CSV file.
     :param database_url: The database connection URL.
     :param chunk_size: Number of rows per chunk.
@@ -77,50 +127,55 @@ def bulk_insert_data_chunked(file_path, database_url, chunk_size=10000):
     total_rows = sum(1 for _ in open(file_path)) - 1  # Subtract 1 for the header row
     num_chunks = (total_rows // chunk_size) + 1
     file_name = os.path.split(file_path)[1]
-    progress_bar = tqdm(total=num_chunks, desc=file_name+'::')
-    
+    progress_bar = tqdm(total=num_chunks, desc=file_name + "::")
+
     # Process chunks and collect all unique navigational statuses
-    for ships_data, ais_data, nav_status_set in read_and_transform_csv_chunk(file_path, chunk_size):
+    for ships_data, ais_data, nav_status_set in read_and_transform_csv_chunk(
+        file_path, chunk_size
+    ):
 
         with bulk_inserter.Session() as session:
             new_nav_status_set = remove_existing_nav_status_ids(session, nav_status_set)
 
-            new_ships_data = remove_existing_ships(session,ships_data)
-        
+            new_ships_data = remove_existing_ships(session, ships_data)
+
         # Insert New Nav Statuses found
         if len(new_nav_status_set) > 0:
             bulk_inserter.bulk_insert(Nav_Status, new_nav_status_set)
 
         # Insert Ships data
-        if len(new_ships_data)>0:
+        if len(new_ships_data) > 0:
             bulk_inserter.bulk_insert(Ships, new_ships_data)
-        
+
         # Insert AIS_Data with navigational status ID conversion
         with bulk_inserter.Session() as session:
-            mmsi2ship_id_map = {ship.mmsi:ship.ship_id for ship in session.query(Ships).all()}
+            mmsi2ship_id_map = {
+                ship.mmsi: ship.ship_id for ship in session.query(Ships).all()
+            }
             nav_status = session.query(Nav_Status).all()
             nav_status_id_map = {status.code: status.id for status in nav_status}
             for ais_record in ais_data:
-                ais_record['navigational_status'] = nav_status_id_map.get(ais_record['navigational_status_text'], 0)
+                ais_record["navigational_status"] = nav_status_id_map.get(
+                    ais_record["navigational_status_text"], 0
+                )
 
-                ais_record['ship_id'] = mmsi2ship_id_map.get(str(ais_record['mmsi']),0)
+                ais_record["ship_id"] = mmsi2ship_id_map.get(str(ais_record["mmsi"]), 0)
 
-                del ais_record['navigational_status_text']
-                del ais_record['mmsi']
-            
+                del ais_record["navigational_status_text"]
+                del ais_record["mmsi"]
 
             bulk_inserter.bulk_insert(AIS_Data, ais_data)
 
         # Update progress bar
         progress_bar.update(1)
-    
-    progress_bar.close()
-    
 
-def remove_existing_nav_status_ids(session, nav_status_set:set):
+    progress_bar.close()
+
+
+def remove_existing_nav_status_ids(session, nav_status_set: set):
     """
     Function to get a map of navigational status text to ID.
-    
+
     :param session: The SQLAlchemy session object.
     :param nav_status_set: Set of unique navigational status texts.
     :return: Dictionary mapping navigational status text to ID.
@@ -130,51 +185,59 @@ def remove_existing_nav_status_ids(session, nav_status_set:set):
 
     new_nav_status = nav_status_set - set(existing_nav_status)
 
-    return [{'code': code} for code in new_nav_status]
+    return [{"code": code} for code in new_nav_status]
+
 
 # FIXME remove duplicate mmsi and verify it!!
 def remove_existing_ships(session, ships_data):
     """
     Function to get a map of mmsi to ship_id.
-    
+
     :param session: The SQLAlchemy session object.
     :param mmsi_set: Set of unique mmsi values.
     :return: Dictionary mapping mmsi to ship_id.
     """
-    
+
     existing_ships = [ship.mmsi for ship in session.query(Ships).all()]
-    
-    if len(existing_ships)>0:
+
+    if len(existing_ships) > 0:
         # Filter ships_data to only include new mmsi
-        ships_data = [ship for ship in ships_data if ship['mmsi'] not in existing_ships]
+        ships_data = [ship for ship in ships_data if ship["mmsi"] not in existing_ships]
     return ships_data
 
-from multiprocessing import Pool, cpu_count
+
 def process_files_in_parallel(csv_files, database_url, chunk_size=50000):
     num_cores = cpu_count()
 
     with Pool(num_cores) as pool:
         # Pass the table name along with the file path for parallel processing
-        pool.starmap(bulk_insert_data_chunked, [(file_path, database_url,chunk_size) for file_path in csv_files])
+        pool.starmap(
+            bulk_insert_data_chunked,
+            [(file_path, database_url, chunk_size) for file_path in csv_files],
+        )
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
 
-    POSTGRES_DB="gis"
-    POSTGRES_USER="clear"
-    POSTGRES_PASSWORD="clear"
-    POSTGRES_PORT=5432
+    POSTGRES_DB = "gis"
+    POSTGRES_USER = "clear"
+    POSTGRES_PASSWORD = "clear"
+    POSTGRES_PORT = 5432
     POSTGRES_HOST = "localhost"
     database_url = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
-    file_path = 'data/ais2018_chemical_tanker.csv'
-    folder_path = 'data/AIS 2023 SFV'
+    file_path = "data/ais2018_chemical_tanker.csv"
+    folder_path = "data/AIS 2023 SFV"
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--datapath', type=str, default=folder_path, help='csv files directory')
-    parser.add_argument('--db_url', type=str, default=database_url, help="Postgres database url")
+    parser.add_argument(
+        "--datapath", type=str, default=folder_path, help="csv files directory"
+    )
+    parser.add_argument(
+        "--db_url", type=str, default=database_url, help="Postgres database url"
+    )
 
-    args =  parser.parse_args()
+    args = parser.parse_args()
 
     path = args.datapath
     if os.path.exists(path):
@@ -185,14 +248,12 @@ if __name__=='__main__':
         if os.path.isfile(path):
             bulk_insert_data_chunked(path, database_url, chunk_size=10000)
         else:
-            csv_files = find_files_in_folder(path, extension=('.csv'))
+            csv_files = find_files_in_folder(path, extension=(".csv"))
             # n_files = len(csv_files)
             # for count, csv_file in enumerate(csv_files):
             #     print(count, '/',n_files, ': ', csv_file)
-                # bulk_insert_data_chunked(csv_file, database_url, chunk_size=50000)
-            process_files_in_parallel(csv_files=csv_files, database_url=database_url, chunk_size=50000)
-                
+            # bulk_insert_data_chunked(csv_file, database_url, chunk_size=50000)
+            process_files_in_parallel(
+                csv_files=csv_files, database_url=database_url, chunk_size=50000
+            )
 
-        
-
-        
